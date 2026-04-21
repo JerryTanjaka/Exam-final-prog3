417ec8398f08a23e9599fde5d1854532906ca125
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-04-21 16:55:06 +0300

    fix : add configuration on datasource config

--- src/main/java/hei/fprog3/datasource/DataSourceConfig.java
+++ src/main/java/hei/fprog3/datasource/DataSourceConfig.java
@@ -6,7 +6,9 @@ import java.sql.SQLException;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DataSourceConfig {
     private final Dotenv dotenv = Dotenv.load();
 
