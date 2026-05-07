5bd3bae4ab224208fe9ec87c3240cbf1ab075605
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-05-07 09:47:11 +0300

    fix : get string payment_id on transaction controller

--- src/main/java/hei/fprog3/repository/TransactionRepository.java
+++ src/main/java/hei/fprog3/repository/TransactionRepository.java
@@ -2,8 +2,10 @@ package hei.fprog3.repository;
 
 import hei.fprog3.datasource.DataSourceConfig;
 import hei.fprog3.exception.NotFoundException;
+import hei.fprog3.model.FinancialAccount;
 import hei.fprog3.model.Payment;
 import hei.fprog3.model.Transaction;
+import hei.fprog3.model.enums.PaymentMethod;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -17,44 +19,63 @@ public class TransactionRepository {
     private DataSourceConfig dataSource;
     private PaymentRepository paymentRepository;
     private MemberRepository memberRepository;
+    private AccountRepository accountRepository;
 
-    public TransactionRepository(DataSourceConfig dataSource, PaymentRepository paymentRepository, MemberRepository memberRepository, CollectivityRepository collectivityRepository) {
+    public TransactionRepository(DataSourceConfig dataSource,
+                                 PaymentRepository paymentRepository,
+                                 MemberRepository memberRepository,
+                                 CollectivityRepository collectivityRepository,
+                                 AccountRepository accountRepository) {
         this.dataSource = dataSource;
         this.paymentRepository = paymentRepository;
         this.memberRepository = memberRepository;
         this.collectivityRepository = collectivityRepository;
+        this.accountRepository = accountRepository;
     }
 
-
-    public List<Transaction> getTransactionBetween(String collectivityId, LocalDate from, LocalDate to) throws NotFoundException {
+    public List<Transaction> getTransactionBetween(String collectivityId,
+                                                   LocalDate from,
+                                                   LocalDate to) throws NotFoundException {
         Connection connection = dataSource.getConnection();
         try {
             collectivityRepository.exists(collectivityId);
+
             List<Transaction> transactions = new ArrayList<>();
-            PreparedStatement transactionsPs = connection.prepareStatement(
-                    """
-                    SELECT p.id, p.member_id, p.creation_date
-                    FROM payments AS p
-                    JOIN accounts AS a ON a.id = p.credited_account_id
-                    WHERE a.collectivity_id = ? AND p.creation_date BETWEEN ? AND ?
-                    """);
+
+            PreparedStatement transactionsPs = connection.prepareStatement("""
+                SELECT
+                    p.id           AS payment_id,
+                    p.member_id,
+                    p.amount,
+                    p.payment_method,
+                    p.creation_date,
+                    p.credited_account_id
+                FROM payments AS p
+                JOIN accounts AS a ON a.id = p.credited_account_id
+                WHERE a.collectivity_id = ?
+                  AND p.creation_date BETWEEN ? AND ?
+                ORDER BY p.creation_date ASC
+                """);
             transactionsPs.setString(1, collectivityId);
             transactionsPs.setDate(2, Date.valueOf(from));
             transactionsPs.setDate(3, Date.valueOf(to));
-            ResultSet rs = transactionsPs.executeQuery();
 
+            ResultSet rs = transactionsPs.executeQuery();
             while (rs.next()) {
                 Transaction transaction = new Transaction();
-                Payment payment = paymentRepository.findById(rs.getString("payment_id"));
-                transaction.setId(rs.getString("id"));
+                transaction.setId(rs.getString("payment_id"));
                 transaction.setCreationDate(rs.getDate("creation_date").toLocalDate());
-                transaction.setAmount(payment.getAmount());
-                transaction.setPaymentMode(payment.getPaymentMode());
-                transaction.setAccountCredited(payment.getAccountCredited());
+                transaction.setAmount(rs.getDouble("amount"));
+                transaction.setPaymentMode(PaymentMethod.valueOf(rs.getString("payment_method")));
+
+                FinancialAccount account = accountRepository.findById(rs.getString("credited_account_id"));
+                transaction.setAccountCredited(account);
                 transaction.setMemberDebited(memberRepository.findById(rs.getString("member_id")));
+
                 transactions.add(transaction);
             }
             return transactions;
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
