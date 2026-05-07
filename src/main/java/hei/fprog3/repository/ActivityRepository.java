20e47348e69111b2459703e6d5c1339b691e0581
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-05-07 13:19:16 +0300

    fix : getAllActivities check if day_of null before setting objet

--- src/main/java/hei/fprog3/repository/ActivityRepository.java
+++ src/main/java/hei/fprog3/repository/ActivityRepository.java
@@ -50,12 +50,24 @@ public class ActivityRepository {
                 activity.setId(activityRs.getString("id"));
                 activity.setLabel(activityRs.getString("label"));
                 activity.setActivityType(ActivityType.valueOf(activityRs.getString("type")));
-                activity.setExecutiveDate(activityRs.getDate("executive_date").toLocalDate());
                 activity.setMemberOccupationConcerned(requiredMembers);
-                activity.setRecurrenceRule(new ActivityRecurrenceRule(
-                        activityRs.getInt("week_ordinal"),
-                        DayOfWeek.valueOf(activityRs.getString("day_of_week")))
-                );
+
+                if (activityRs.getObject("executive_date") != null) {
+                    activity.setExecutiveDate(activityRs.getDate("executive_date").toLocalDate());
+                } else {
+                    activity.setExecutiveDate(null);
+                }
+
+                if (activityRs.getObject("week_ordinal") != null
+                        && activityRs.getObject("day_of_week") != null) {
+                    activity.setRecurrenceRule(new ActivityRecurrenceRule(
+                            activityRs.getInt("week_ordinal"),
+                            DayOfWeek.valueOf(activityRs.getString("day_of_week")))
+                    );
+                } else {
+                    activity.setRecurrenceRule(null);
+                }
+
                 activities.add(activity);
             }
             return activities;
@@ -65,7 +77,6 @@ public class ActivityRepository {
             dataSource.closeConnection(connection);
         }
     }
-
     public List<Activity> createActivityAndReturn(String id, List<ActivityCreate> newActivities) {
         Connection connection = dataSource.getConnection();
         try {
